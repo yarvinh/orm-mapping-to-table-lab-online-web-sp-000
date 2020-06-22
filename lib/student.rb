e0b8bd5e9ebad3a5fcd328d2bfefe26f77900e31
Yarvin Hernandez <yarvinhhernandez@gmail.com>
2020-06-22 18:36:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,8 +39,9 @@ class Student
     grade = []
      hash.each{|k,v| k == :name ? name << v :   grade << v }
      name.each_with_index{|name,index|  Student.new(name,grade[index])
+       p self
    }
-    p self
+
   end
 
 end
